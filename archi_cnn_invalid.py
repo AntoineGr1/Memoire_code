e1937eb72bc3b017ce98b3fcd792098afa954bf0
AntoineGr1 <antoine.gratia@student.unamur.be>
2021-03-08 10:08:31 +0100

    poc compilateur

---
+++ archi_cnn_invalid.py
@@ -0,0 +1,13 @@
+import keras
+import numpy as np
+from keras.utils import plot_model
+model = keras.models.Sequential([
+	keras.layers.Flatten(),
+	keras.layers.AveragePooling2D(pool_size=2, strides=None, padding='valid'),
+	keras.layers.Conv2D(16, kernel_size=5, strides=1, activation='tanh', padding='valid'),
+	keras.layers.Conv2D(6, kernel_size=5, strides=1,  activation='tanh', input_shape=(28, 28, 1), padding='same'),
+	keras.layers.Dense(84, activation='tanh'),
+	keras.layers.Dense(10, activation='softmax')
+
+])
+plot_model(model, show_shapes=True, to_file='test_auto_code_invalid.png')

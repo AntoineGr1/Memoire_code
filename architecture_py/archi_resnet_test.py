7ef96e88d01d892e340ca66d3ad4f3443001deb1
AntoineGr1 <antoine.gratia@student.unamur.be>
2021-04-06 10:51:50 +0200

    test fonctionnement generator

--- architecture_py/archi_resnet_test.py
+++ architecture_py/archi_resnet_test.py
@@ -75,24 +75,25 @@ try:
         return X
     def ResNet():
         X_input = X = Input([28, 28, 1])
-        X = Conv2D(18, kernel_size=7, strides=2, activation='relu', padding='same')(X)
+        X = Conv2D(18, kernel_size=7, strides=2, activation='tanh', padding='valid')(X)
         X = AveragePooling2D(pool_size=3, strides=2, padding='valid')(X)
-        X = id_block(X, 3, 18)
         X = conv_block(X, 3, 36, 2)
         X = id_block(X, 3, 36)
         X = conv_block(X, 3, 72, 2)
+        X = id_block(X, 3, 72)
+        X = conv_block(X, 3, 144, 2)
         model = Model(inputs=X_input, outputs=X, name='ResNet18')
         return model
 
     Input = ResNet()
     head_model = Input.output
     head_model = Flatten()(head_model)
-    head_model = Dense(21, activation='selu')(head_model)
-    head_model = Dense(15, activation='selu')(head_model)
+    head_model = Dense(49, activation='tanh')(head_model)
     head_model = Dense(10, activation='softmax')(head_model)
     model = Model(inputs=Input.input, outputs=head_model)
     plot_model(model, show_shapes=True, to_file="../architecture_img/archi_resnet_test.png")
     model.compile(optimizer='adam', loss=keras.losses.sparse_categorical_crossentropy, metrics=['accuracy'])
+    
     start = time()
     model.fit(train_x, train_y, epochs=5, validation_data=(val_x, val_y))
     training_time = time()-start
